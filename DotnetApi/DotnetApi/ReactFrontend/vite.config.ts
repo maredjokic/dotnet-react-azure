778cf7f53fc05baa77dff02ca703d3a206203351
Marko Djokic <mare160696@gmail.com>
2025-08-01 11:06:55 +0200

    fix config to deploy on vite

--- DotnetApi/DotnetApi/ReactFrontend/vite.config.ts
+++ DotnetApi/DotnetApi/ReactFrontend/vite.config.ts
@@ -6,7 +6,7 @@ export default defineConfig({
     plugins: [react(), tailwindcss()],
     base: '/',
     build: {
-        outDir: '../wwwroot',
+        outDir: 'dist', // '../wwwroot', 
         emptyOutDir: true,
     },
 })
