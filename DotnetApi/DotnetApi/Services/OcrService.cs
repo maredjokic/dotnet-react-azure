154fbe3349fbaeed9f0ea5ba339acfc0df69ebb9
Marko Djokic <mare160696@gmail.com>
2025-08-14 14:05:32 +0200

    fix

--- DotnetApi/DotnetApi/Services/OcrService.cs
+++ DotnetApi/DotnetApi/Services/OcrService.cs
@@ -2,43 +2,38 @@
 using DotnetApi.Services.Interfaces;
 using Tesseract;
 
-namespace DotnetApi.Services
+public class OcrService : IOcrService
 {
-    public class OcrService : IOcrService
+    private readonly string _tessDataPath;
+
+    public OcrService(IWebHostEnvironment env)
     {
-        private readonly string _tessDataPath;
-
-        public OcrService()
+        if (env.IsDevelopment())
         {
-            var env = Environment.GetEnvironmentVariable("ASPNETCORE_ENVIRONMENT");
-
-            if (env == "Development")
-            {
-                _tessDataPath = Path.Combine(Directory.GetCurrentDirectory(), "tessdata");
-            }
-            else
-            {
-                _tessDataPath = "/home/site/wwwroot/tessdata";
-            }
+            _tessDataPath = Path.Combine(Directory.GetCurrentDirectory(), "tessdata");
         }
-
-        public async Task<OcrResultDto> ExtractTextAsync(Stream imageStream, string language)
+        else
         {
-            var imageBytes = await ReadFullyAsync(imageStream);
-
-            using var engine = new TesseractEngine(_tessDataPath, language, EngineMode.Default);
-            using var img = Pix.LoadFromMemory(imageBytes);
-            using var page = engine.Process(img);
-
-            var text = page.GetText();
-            return new OcrResultDto { Text = text };
+            _tessDataPath = "/home/site/wwwroot/tessdata";
         }
+    }
 
-        private async Task<byte[]> ReadFullyAsync(Stream input)
-        {
-            using var ms = new MemoryStream();
-            await input.CopyToAsync(ms);
-            return ms.ToArray();
-        }
+    public async Task<OcrResultDto> ExtractTextAsync(Stream imageStream, string language)
+    {
+        var imageBytes = await ReadFullyAsync(imageStream);
+
+        using var engine = new TesseractEngine(_tessDataPath, language, EngineMode.Default);
+        using var img = Pix.LoadFromMemory(imageBytes);
+        using var page = engine.Process(img);
+
+        var text = page.GetText();
+        return new OcrResultDto { Text = text };
+    }
+
+    private async Task<byte[]> ReadFullyAsync(Stream input)
+    {
+        using var ms = new MemoryStream();
+        await input.CopyToAsync(ms);
+        return ms.ToArray();
     }
 }
