c875fa09fce9d5db8c43ad10ec0a47931ba904f4
Marko Djokic <mare160696@gmail.com>
2025-08-15 07:26:04 +0200

    add library path

--- DotnetApi/DotnetApi/startup.sh
+++ DotnetApi/DotnetApi/startup.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
 apt-get update
 apt-get install -y tesseract-ocr libtesseract-dev libleptonica-dev
+export LD_LIBRARY_PATH=/usr/lib/x86_64-linux-gnu:$LD_LIBRARY_PATH
 dotnet DotnetApi.dll
