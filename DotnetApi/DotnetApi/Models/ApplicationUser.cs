70e05bc6e554a6964c9f0e7092d97b675ae63416
Marko Djokic <mare160696@gmail.com>
2025-08-19 14:29:08 +0200

    add user props

--- DotnetApi/DotnetApi/Models/ApplicationUser.cs
+++ DotnetApi/DotnetApi/Models/ApplicationUser.cs
@@ -4,6 +4,8 @@ namespace DotnetApi.Models
 {
     public class ApplicationUser : IdentityUser
     {
-        public string? FullName { get; set; }
+        public string? FirstName { get; set; }
+        public string? LastName { get; set; }
+        public string? Address { get; set; }
     }
 }
